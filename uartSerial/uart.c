6dffc18c8851a73a4583e9aa284911b15a3dd560
Nayanda Haberty <naysone@yahoo.com>
2017-02-22 12:45:00 +0700

    first push

---
+++ uartSerial/uart.c
@@ -0,0 +1,91 @@
+/*
+* uart.c
+*
+* Created: 6/9/2015 11:44:27 PM
+* Author: Nayanda Haberty <nayanda1@outlook.com>
+*/
+
+#include <avr/io.h>
+#include "uart.h"
+#include <util/delay.h>
+
+SerialPort uartInit(uint8_t *ubrrL, uint8_t *ubrrH, uint8_t *ucsrA, uint8_t *ucsrB, uint8_t *ucsrC, uint8_t *udr){
+	SerialPort port;
+	port.ubrrL = ubrrL;
+	port.ubrrH = ubrrH;
+	port.ucsrA = ucsrA;
+	port.ucsrB = ucsrB;
+	port.ucsrC = ucsrC;
+	port.udr = udr;
+	port.timeOut = calculateTimeOut(*ubrrL, *ubrrH);
+	return port;
+}
+
+void uartStandardInit(SerialPort *port, unsigned int baud){
+	unsigned int ubrr = F_CPU / 16 / baud - 1;
+	*(port->ubrrH) = ubrr >> 7;
+	*(port->ubrrL) = ubrr;
+	*(port->ucsrB) = ((1 << RX_ENABLE) | (1 << TX_ENABLE));			 //enable TX0 and RX0
+	*(port->ucsrC) = ((1 << U_STOP_BIT) | (1 << UCSZ0) | (1 << UCSZ1)); // set 8 bit data and 2 bit stop bit
+	port->timeOut = calculateTimeOut(*(port->ubrrL), *(port->ubrrH));
+}
+
+void uartClose(SerialPort *port){
+	*(port->ucsrB) = 0x00;
+}
+
+char uartReadChar(SerialPort *port){
+	if(uartTimeout(port)){
+		return 0x00;
+	}
+	return *(port->udr);
+}
+
+void uartSendChar(SerialPort *port, const char character){
+	while (!(*(port->ucsrA) & (1 << UDR_READY)))
+	;
+	*(port->udr) = character;
+}
+
+void uartPrint(SerialPort *port, const char *string){
+	while (*string != 0x00){
+		uartSendChar(port, *string);
+		string++;
+	}
+}
+
+void uartScan(SerialPort *port, char *str, unsigned int length){
+	unsigned int i = 0, timeout = 0;
+	while (!timeout && i < length){
+		*str = uartReadChar(port);
+		timeout = uartTimeout(port);
+		str++;
+		i++;
+	}
+}
+
+boolean uartAvailable(SerialPort *port){
+	return (*(port->ucsrA) & (1 << RX_FLAG));
+}
+
+boolean uartTimeout(SerialPort *port){
+	int time = 0;
+	while (!uartAvailable(port) && time < 100){
+		_delay_us(port->timeOut);
+		time++;
+	}
+	return (time > 1000);
+}
+
+void flush(SerialPort *port){
+	char dummy;
+	while (*(port->ucsrA) & (1<<RX_FLAG)) dummy = *(port->udr);
+	return;
+}
+
+unsigned long calculateTimeOut(uint8_t ubrrL, uint8_t ubrrH){
+	unsigned int ubrr = ubrrL;
+	ubrr = ubrr | (ubrrH >> 7);
+	unsigned long baud = ((F_CPU / 16) / (ubrr + 1));
+	return (1000000UL / baud);
+}
---
+++ uartSerial/uart.h
@@ -0,0 +1,152 @@
+/*
+* uart.h
+*
+* Created: 6/9/2015 11:46:39 PM
+* Author: Nayanda Haberty <nayanda1@outlook.com>
+*/
+
+
+#ifndef UART_H_
+#define UART_H_
+#define __DELAY_BACKWARD_COMPATIBLE__
+#endif /* PCSERIAL_H_ */
+
+#ifndef boolean
+#define boolean unsigned char
+#endif
+
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+
+#define RX_ENABLE 4
+#define TX_ENABLE 3
+#define U_STOP_BIT 3
+
+#ifndef UCSZ0
+#define UCSZ0 1
+#define UCSZ1 2
+#endif
+
+#define RX_FLAG 7
+#define UDR_READY 5
+
+typedef struct SerialPort{
+	volatile uint8_t *ubrrL;
+	volatile uint8_t *ubrrH;
+	volatile uint8_t *ucsrA;
+	volatile uint8_t *ucsrB;
+	volatile uint8_t *ucsrC;
+	volatile uint8_t *udr;
+	unsigned long timeOut;
+} SerialPort;
+
+/**
+ * \brief 
+ * 
+ * \param ubrrL
+ * \param ubrrH
+ * \param ucsrA
+ * \param ucsrB
+ * \param ucsrC
+ * \param udr
+ * 
+ * \return SerialPort
+ */
+SerialPort uartInit(uint8_t *ubrrL, uint8_t *ubrrH, uint8_t *ucsrA, uint8_t *ucsrB, uint8_t *ucsrC, uint8_t *udr);
+
+/**
+ * \brief 
+ * 
+ * \param port
+ * \param baud
+ * 
+ * \return void
+ */
+void uartStandardInit(SerialPort *port, unsigned int baud);
+
+/**
+ * \brief 
+ * 
+ * \param port
+ * 
+ * \return void
+ */
+void uartClose(SerialPort *port);
+
+/**
+ * \brief 
+ * 
+ * \param port
+ * \param string
+ * 
+ * \return void
+ */
+void uartPrint(SerialPort *port, const char *string);
+
+/**
+ * \brief 
+ * 
+ * \param port
+ * \param string
+ * \param length
+ * 
+ * \return void
+ */
+void uartScan(SerialPort *port, char *string, unsigned int length);
+
+/**
+ * \brief 
+ * 
+ * \param port
+ * 
+ * \return boolean
+ */
+boolean uartAvailable(SerialPort *port);
+
+/**
+ * \brief 
+ * 
+ * \param port
+ * 
+ * \return boolean
+ */
+boolean uartTimeout(SerialPort *port);
+
+/**
+ * \brief 
+ * 
+ * \param port
+ * 
+ * \return char
+ */
+char uartReadChar(SerialPort *port);
+
+/**
+ * \brief 
+ * 
+ * \param port
+ * \param character
+ * 
+ * \return void
+ */
+void uartSendChar(SerialPort *port, const char character);
+
+/**
+ * \brief 
+ * 
+ * \param port
+ * 
+ * \return void
+ */
+void flush(SerialPort *port);
+
+/**
+ * \brief 
+ * 
+ * \param ubrrL
+ * \param ubrrH
+ * 
+ * \return unsigned long
+ */
+unsigned long calculateTimeOut(uint8_t ubrrL, uint8_t ubrrH);
